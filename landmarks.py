a4f0d1427d1e97ea44d7972b7eb318748310a8b2
Alexander Kurz <alex.kurz95@gmail.com>
2024-10-10 12:01:37 +0200

    Compute landmarks with ADNet

---
+++ landmarks.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import onnxruntime as ort
+
+
+def draw_landmarks(img: np.ndarray, landmarks: np.ndarray):
+    for idx in range(len(landmarks)):
+        x, y = landmarks[idx]
+        cv2.circle(img, (x, y), 1, (255, 0, 0), cv2.FILLED)
+        cv2.putText(img, str(idx), (x + 2, y), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 0, 0))
+    cv2.imshow("Landmarks", img)
+    cv2.waitKey(0)
+
+
+def main(
+    img_p: str = "data/c-07-twofaces_cropped.png",
+    model_p: str = "checkpoints/adnet/adnet_ofiq.onnx",
+):
+    # Load model
+    # model = cv2.dnn.readNetFromONNX(model_p)  # Cannot read ADNet model with OpenCV
+    ort_sess = ort.InferenceSession(model_p)
+    input_size = (256, 256)
+
+    # Set up data
+    img = cv2.imread(img_p)
+    blob = cv2.dnn.blobFromImage(
+        img,
+        scalefactor=1 / (127.5),
+        size=input_size,
+        mean=(127.5, 127.5, 127.5),
+        swapRB=True,
+        crop=False,
+    )
+
+    # Run forward pass
+    outputs = ort_sess.run(["output"], {"input": blob})
+    landmarks: np.ndarray = outputs[0]
+    landmarks = landmarks.squeeze()  # Squeeze out batch dimension
+    print(landmarks.shape)
+    # De-normalize landmarks
+    landmarks = 127.5 * (landmarks + 1)
+    landmarks = landmarks.astype("int")
+    draw_landmarks(img, landmarks)
+
+
+if __name__ == "__main__":
+    main()
